f0c8934032479d8668bc7b51d4aca4661c83b042
Detective Pikachu <frenzyexists@gmail.com>
2024-01-06 14:07:12 -0400

    Update bots.py

--- bots.py
+++ bots.py
@@ -4,11 +4,13 @@ import keyboard as kewb
 import win32api
 import win32con
 import os
+from itertools import product
 
-def click(x: int, y: int):
+
+def click(x: int, y: int, delay: int):
     win32api.SetCursorPos((x, y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
-    time.sleep(0.01)
+    time.sleep(delay)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
 
@@ -18,20 +20,23 @@ def pianoTileReflex():
 
     while not kewb.is_pressed('q'):
         x, y = 1274, 470  # starting point
-        for i in range(0, 4):
-            for j in range(0, 4):
-                if auto.pixel(x+i*100, y+j*100) == gray:
-                    click(x+i*100, y+j*100)
+        for i, j in product(range(0, 4), range(0, 4)):
+            if auto.pixel(x+i*100, y+j*100) == gray:
+                click(x+i*100, y+j*100, 0.01)
 
 
 def pianoTileTap():
     # https://lagged.com/en/g/magic-tiles
     black = (0, 0, 0)
     while not kewb.is_pressed('q'):
-        x, y = (1240, 520)
-        for i in range(0, 4):
-            if auto.pixel(x+i*100+40, y) == black:
-                click(x+i*100+40, y)
+        if auto.pixel(1240, 400) == black:
+            click(1240, 400, 0.1)
+        if auto.pixel(1380, 400) == black:
+            click(1380, 400, 0.1)
+        if auto.pixel(1490, 400) == black:
+            click(1490, 400, 0.1)
+        if auto.pixel(1640, 400) == black:
+            click(1640, 400, 0.1)
 
 
 def rolling():
@@ -42,22 +47,27 @@ def rolling():
     while not kewb.is_pressed('q'):
         if auto.pixel(1400, 540) == red:
             if auto.pixel(1280, 416) == red or auto.pixel(1280, 768) == red:
-                time.sleep(0.019*multiplier)
-            click(1400, 540)
+                time.sleep(0.018*multiplier)
+            click(1400, 540, 0.05)
             time.sleep(abs(0.019+0.09*multiplier))
         et = time.time()
-        # print(et - st)
-        print(abs(0.019+0.09*multiplier))
+        # print(abs(0.019+0.09*multiplier))
         if et - st >= 19:
             multiplier = abs(multiplier - 0.056)
             st = time.time()
 
 
 def imPath(filename):
-    """A shortcut for joining the 'images/'' file path, since it is used so often. Returns the filename with 'images/' prepended."""
+    """
+    A shortcut for joining the 'images/'' file path, 
+    since it is used so often. Returns the filename with 
+    'images/' prepended.
+    """
     return os.path.join('images', filename)
 
 
+# Not for use, just something I was experimenting with and
+# did not bothered to remove
 def stickmanTest():
     # force use of ImageNotFoundException
     auto.useImageNotFoundException()
@@ -85,6 +95,7 @@ to detect so it matches with your screen
 3. Rolling game: https://lagged.com/en/g/rolling
       """)
 a = input("choose which bot to try Ej. 3: ")
+print("Remember to press 'q' to exit!")
 if int(a) == 1:
     time.sleep(2)
     pianoTileReflex()
